a745255b7f989014ae80fa23584a3b6d1cbf81bc
Varun Malyala <varunmly@gmail.com>
2022-02-04 20:18:03 +0530

    Using busboy + part encoding

--- frontend/script.js
+++ frontend/script.js
@@ -1,25 +1,83 @@
 const form = document.getElementById("form");
 
-form.addEventListener("submit", submitForm);
+const submitForm = async (e) => {
+    e.preventDefault();
+    const name = document.getElementById("name");
+    const files = document.getElementById("files");
 
-function submitForm(e) {
-	e.preventDefault();
-	const name = document.getElementById("name");
-	const files = document.getElementById("files");
+    const formData = new FormData();
 
-	const formData = new FormData();
-    
     formData.append("name", name.value);
 
-    for(let i =0; i < files.files.length; i++) {
-        formData.append("files", files.files[i]);
+    for (let i = 0; i < files.files.length; i++) {
+        try {
+            let x = await splitEncode(files.files[i]);
+			let filename = files.files[i].name
+            console.log(x);
+            let blob = new Blob([JSON.stringify(x)], {type : "application/json"})
+            formData.append("files", blob, filename);
+        } catch (error) {
+            console.log(error);
+            throw error
+        }
     }
 
+    fetch("http://localhost:5000/upload_files", {
+        method: "post",
+        body: formData,
+    })
+        .then((res) => console.log(res))
+        .catch((err) => ("Error occured", err));
+};
 
-	fetch("http://localhost:5000/upload_files", {
-		method: "post",
-		body: formData,
-	})
-		.then((res) => console.log(res))
-		.catch((err) => ("Error occured", err));
+function blobToBase64(blob) {
+    var reader = new FileReader();
+
+    reader.readAsDataURL(blob);
+    reader.onloadend = function () {
+        var base64data = reader.result;
+        console.log(base64data);
+        return;
+    };
 }
+
+const splitEncode = async (file) => {
+    const chunkSize = 60000; // 50KB
+    const filename = file.name;
+    console.log("FILE - ", filename);
+    const numberOfChunks = Math.ceil(file.size / chunkSize);
+    let start = 0,
+        end = 0;
+    let chunk, base64String;
+    let x = [];
+    for (let index = 0; index < numberOfChunks; index++) {
+        start = index * chunkSize;
+        end = Math.min(start + chunkSize, file.size);
+
+        chunk = file.slice(start, end);
+        console.log("CHUNKS - ", start, end);
+
+        function blobToBase64(blob) {
+            return new Promise((resolve, _) => {
+                const reader = new FileReader();
+                reader.onloadend = () => resolve(reader.result);
+                reader.readAsDataURL(blob);
+            });
+        }
+
+        x.push(blobToBase64(chunk));
+    }
+    try {
+        const base64Arr = await Promise.all(x).then((data) => data);
+        return base64Arr;
+    } catch (error) {
+        console.log(err);
+    }
+};
+
+form.addEventListener("submit", submitForm);
+// FILE -> 1MB
+// Create chunks 50KB (200 chunks)
+// CHUNK -> BASE64ENCODE -> PUSH TO ARR
+// obj = {ARR: ARR, FILENAME: FILE.NAME, NUM_CHUNKS: 200}
+// FormData.append(filename, blob(obj))
--- server.js
+++ server.js
@@ -1,20 +1,69 @@
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const { randomFillSync } = require("crypto");
+const fs = require("fs");
+const http = require("http");
+const os = require("os");
+const path = require("path");
 
-const express = require("express");
+const busboy = require("busboy");
 
-const app = express();
+const random = (() => {
+  const buf = Buffer.alloc(16);
+  return () => randomFillSync(buf).toString("hex");
+})();
 
-app.use(express.json());
+http.createServer((req, res) => {
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+  res.setHeader("Access-Control-Max-Age", 2592000); // 30 days
 
-app.post("/upload_files", upload.array("files"), uploadFiles);
-
-function uploadFiles(req, res) {
-	console.dir(req.body);
-	console.log(req.files);
-	res.json({ message: "Successfully uploaded files" });
-}
-
-app.listen(5000, () => {
-	console.log(`Server started...`);
+  if (req.method === "POST") {
+    const bb = busboy({ headers: req.headers });
+    bb.on("file", (name, file, info) => {
+      const { filename, encoding, mimeType } = info;
+      console.log(
+        `File [${name}]: filename: %j, encoding: %j, mimeType: %j`,
+        filename,
+        encoding,
+        mimeType
+      );
+      let chunks = [];
+      let merged = Buffer.from("");
+      let finalData,
+        decodedData = Buffer.from("");
+      file.on("data", (data) => {
+        chunks.push(data);
+        console.log(
+          `File [${name}] got ${data.length} bytes, chunk : ${chunks.length}`
+        );
+      }).on("close", () => {
+        console.log(`File [${name}] done`);
+        for (chunk of chunks) {
+          merged = Buffer.concat([merged, chunk]);
+        }
+        finalData = JSON.parse(merged.toString());
+        let corrected;
+        for (let part of finalData) {
+          corrected = part.split(",")[1];
+          partBuffer = Buffer.from(corrected, "base64");
+          console.log("Part Buffer : ", partBuffer.length);
+          decodedData = Buffer.concat([decodedData, partBuffer]);
+        }
+        console.log(decodedData.length, decodedData);
+        fs.writeFile("./uploads/" + filename, decodedData, (err) => {
+          if (err) throw err;
+        });
+      });
+    });
+    bb.on("close", () => {
+      res.writeHead(200, { Connection: "close" });
+      res.end(`That's all folks!`);
+    });
+    req.pipe(bb);
+    return;
+  }
+  res.writeHead(404);
+  res.end();
+}).listen(5000, () => {
+  console.log("Listening for requests on 5000");
 });
